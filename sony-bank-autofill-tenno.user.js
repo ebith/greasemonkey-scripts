5a2077336a7077a109e616e12ac655242283673b
ebith <ebith.h@gmail.com>
2018-11-03 21:58:39 +0900

    ソニー銀行の店番号を自動入力する

---
+++ sony-bank-autofill-tenno.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Sony Bank auto fill TenNo
+// @version      0.1
+// @author       ebith
+// @match        https://o2o.moneykit.net/NBG100001G01.html
+// @grant        none
+// ==/UserScript==
+
+(function() {
+ document.querySelector('input[name=TenNo]').value='001'
+})();
