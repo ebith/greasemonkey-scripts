3c8f54eaa0b60bd26836beea5a2642c96090f653
ebith <ebith.h@gmail.com>
2023-01-13 06:52:46 +0900

    整理した

--- nicovideo-ng.user.js
+++ nicovideo-ng.user.js
@@ -6,31 +6,31 @@
 // @version     1.0
 // @author      ebith
 // ==/UserScript==
-;(async () => {
-  const ngUsers = await GM.getValue('ngUsers', [])
+(async () => {
+  const ngUsers = await GM.getValue('ngUsers', []);
 
   document.querySelectorAll('.NC-VideoMediaObjectWrapper').forEach(async (video) => {
-    const button = document.createElement('button')
-    button.textContent = 'NG Userに追加する'
-    button.className = 'VideoMenu-Item'
-    button.dataset.userId = video.dataset.ownerId
+    const button = document.createElement('button');
+    button.textContent = 'NGユーザに追加する';
+    button.className = 'VideoMenu-Item';
+    button.dataset.userId = video.dataset.ownerId;
     button.addEventListener('click', async (event) => {
-      ngUsers.push(button.dataset.userId)
-      await GM.setValue('ngUsers', ngUsers)
+      ngUsers.push(button.dataset.userId);
+      await GM.setValue('ngUsers', ngUsers);
 
-      const menu = event.target.closest('.NC-VideoMediaObject .NC-ThreePointMenu-menu')
-      menu.hidden = true
-      const video = event.target.closest('.NC-VideoMediaObjectWrapper')
-      video.dataset.muted = ''
-      video.hidden = true
-    })
-    const menu = video.querySelector('.NC-VideoMediaObject .NC-ThreePointMenu-menu')
-    menu.querySelector('.VideoMenu-item_mute').remove()
-    menu.appendChild(button)
+      const menu = event.target.closest('.NC-VideoMediaObject .NC-ThreePointMenu-menu');
+      menu.hidden = true;
+      const video = event.target.closest('.NC-VideoMediaObjectWrapper');
+      video.dataset.muted = '';
+      video.hidden = true;
+    });
+    const menu = video.querySelector('.NC-VideoMediaObject .NC-ThreePointMenu-menu');
+    menu.querySelector('.VideoMenu-item_mute').remove();
+    menu.appendChild(button);
 
     if (ngUsers.includes(video.dataset.ownerId)) {
-      video.dataset.muted = ''
-      video.hidden = true
+      video.dataset.muted = '';
+      video.hidden = true;
     }
-  })
-})()
+  });
+})();
